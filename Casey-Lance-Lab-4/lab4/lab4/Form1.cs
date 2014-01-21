a4a418ded514cf78bd23e1ecee54cb2943ef53bc
caseylance <caseylance@gmail.com>
2014-01-20 19:49:05 -0700

    Lab 4

---
+++ Casey-Lance-Lab-4/lab4/lab4/Form1.cs
@@ -0,0 +1,44 @@
+﻿//Casey Lance
+//CS 1400
+//Lab #4
+//---------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // The exitToolStripMenuItem1 method
+        // Purpose: To close the window and terminate the application
+        // Parameters: The object generating the event 
+        // and the event arguments
+        // Returns: None
+        private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        // The aboutToolStripMenuItem method
+        // Purpose: To display my intentifying information
+        // Parameters: The object generating the event 
+        // and the event arguments
+        // Returns: Window displaying my contat information
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Casey Lance\nCS1400\nLab #4");
+        }
+    }
+}
