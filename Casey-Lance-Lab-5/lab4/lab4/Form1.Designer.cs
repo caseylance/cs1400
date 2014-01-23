c187dccead3eac55504aed12d7166988ac360cfa
caseylance <caseylance@gmail.com>
2014-01-22 20:05:27 -0700

    Submitting Lab 5

---
+++ Casey-Lance-Lab-5/lab4/lab4/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿//Casey Lance
+//CS 1400
+//Lab #4
+//---------------------------------------------------------------------
+
+namespace lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.inTxtBox = new System.Windows.Forms.TextBox();
+            this.labe1 = new System.Windows.Forms.Label();
+            this.outTxtBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(226, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.exitToolStripMenuItem1});
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // exitToolStripMenuItem1
+            // 
+            this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(107, 22);
+            this.exitToolStripMenuItem1.Text = "Exit";
+            this.exitToolStripMenuItem1.Click += new System.EventHandler(this.exitToolStripMenuItem1_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(82, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Process";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // inTxtBox
+            // 
+            this.inTxtBox.Location = new System.Drawing.Point(82, 47);
+            this.inTxtBox.Name = "inTxtBox";
+            this.inTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.inTxtBox.TabIndex = 2;
+            // 
+            // labe1
+            // 
+            this.labe1.AutoSize = true;
+            this.labe1.Location = new System.Drawing.Point(38, 54);
+            this.labe1.Name = "labe1";
+            this.labe1.Size = new System.Drawing.Size(34, 13);
+            this.labe1.TabIndex = 3;
+            this.labe1.Text = "Input:";
+            // 
+            // outTxtBox
+            // 
+            this.outTxtBox.Location = new System.Drawing.Point(82, 105);
+            this.outTxtBox.Name = "outTxtBox";
+            this.outTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.outTxtBox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Output:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(226, 204);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.outTxtBox);
+            this.Controls.Add(this.labe1);
+            this.Controls.Add(this.inTxtBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Lab 05";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox inTxtBox;
+        private System.Windows.Forms.Label labe1;
+        private System.Windows.Forms.TextBox outTxtBox;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Casey-Lance-Lab-5/lab4/lab4/Form1.cs
@@ -0,0 +1,59 @@
+﻿//Casey Lance
+//CS 1400
+//Lab #4
+//---------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // The exitToolStripMenuItem1 method
+        // Purpose: To close the window and terminate the application
+        // Parameters: The object generating the event 
+        // and the event arguments
+        // Returns: None
+        private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        // The aboutToolStripMenuItem method
+        // Purpose: To display my intentifying information
+        // Parameters: The object generating the event 
+        // and the event arguments
+        // Returns: Window displaying my contat information
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Casey Lance\nCS1400\nLab #5");
+        }
+
+        // The button1_Click Method
+        // Purpose: Get a value from the user and display it back again
+        // Parameters: The sending object, and the event arguments
+        // Returns: none
+           
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            {
+                int num = int.Parse(inTxtBox.Text);
+                string outStr = string.Format("{0:D}", num);
+                outTxtBox.Text = outStr;
+            }
+        } 
+    }
+}
