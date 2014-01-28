8de7187f5a7a7554753cc2d70cd01612dbc29535
caseylance <caseylance@gmail.com>
2014-01-27 17:01:35 -0700

    Lab 07

---
+++ Casey-Lance-Lab-7/lab7/lab7/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace lab7
+{
+    partial class totalBenevolentGold
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.totalGoldTxtBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.totalPiratesTxtBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.totalCaptainsGoldTxtBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.totalFirstOfficersGoldTxtBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.totalCrewMemberGoldTxtBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.totalBenevolentFundGoldTxtBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // totalGoldTxtBox
+            // 
+            this.totalGoldTxtBox.Location = new System.Drawing.Point(210, 68);
+            this.totalGoldTxtBox.Name = "totalGoldTxtBox";
+            this.totalGoldTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.totalGoldTxtBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Total Pieces of Gold on Ship:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Total Pirates on Ship:";
+            // 
+            // totalPiratesTxtBox
+            // 
+            this.totalPiratesTxtBox.Location = new System.Drawing.Point(210, 94);
+            this.totalPiratesTxtBox.Name = "totalPiratesTxtBox";
+            this.totalPiratesTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.totalPiratesTxtBox.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Captains Share of Gold Pieces:";
+            // 
+            // totalCaptainsGoldTxtBox
+            // 
+            this.totalCaptainsGoldTxtBox.Location = new System.Drawing.Point(210, 179);
+            this.totalCaptainsGoldTxtBox.Name = "totalCaptainsGoldTxtBox";
+            this.totalCaptainsGoldTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.totalCaptainsGoldTxtBox.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(171, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "First Officers Share of Gold Pieces:";
+            // 
+            // totalFirstOfficersGoldTxtBox
+            // 
+            this.totalFirstOfficersGoldTxtBox.Location = new System.Drawing.Point(210, 205);
+            this.totalFirstOfficersGoldTxtBox.Name = "totalFirstOfficersGoldTxtBox";
+            this.totalFirstOfficersGoldTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.totalFirstOfficersGoldTxtBox.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 234);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(146, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Total Gold Per Crew Member:";
+            // 
+            // totalCrewMemberGoldTxtBox
+            // 
+            this.totalCrewMemberGoldTxtBox.Location = new System.Drawing.Point(210, 231);
+            this.totalCrewMemberGoldTxtBox.Name = "totalCrewMemberGoldTxtBox";
+            this.totalCrewMemberGoldTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.totalCrewMemberGoldTxtBox.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(220, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Process!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(220, 301);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Clear!";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 260);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(192, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Total Gold for Pirate\'s BenevolentFund:";
+            // 
+            // totalBenevolentFundGoldTxtBox
+            // 
+            this.totalBenevolentFundGoldTxtBox.Location = new System.Drawing.Point(210, 257);
+            this.totalBenevolentFundGoldTxtBox.Name = "totalBenevolentFundGoldTxtBox";
+            this.totalBenevolentFundGoldTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.totalBenevolentFundGoldTxtBox.TabIndex = 12;
+            // 
+            // totalBenevolentGold
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 407);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.totalBenevolentFundGoldTxtBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.totalCrewMemberGoldTxtBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.totalFirstOfficersGoldTxtBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.totalCaptainsGoldTxtBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.totalPiratesTxtBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.totalGoldTxtBox);
+            this.Name = "totalBenevolentGold";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox totalGoldTxtBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox totalPiratesTxtBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox totalCaptainsGoldTxtBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox totalFirstOfficersGoldTxtBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox totalCrewMemberGoldTxtBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox totalBenevolentFundGoldTxtBox;
+    }
+}
+
---
+++ Casey-Lance-Lab-7/lab7/lab7/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab7
+{
+    public partial class totalBenevolentGold : Form
+    {
+        public totalBenevolentGold()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Get total gold on ship
+            double totalGold = double.Parse(totalGoldTxtBox.Text);
+
+            //Get total number of pirates on ship
+            double totalPirates = double.Parse(totalPiratesTxtBox.Text);
+
+            //Get total number of pirates that are not the captain or first officer
+            double totalCrew = totalPirates - 2;
+
+            //Get total amount of gold given to the crew for town
+            double totalCrewMembersTownGold = totalCrew * 3;
+
+            //Get remaining gold after giving gold to crew for town
+            double totalAfterCrewMemberTownGold = totalGold - totalCrewMembersTownGold;
+
+            //Get twelve percent of gold for captain
+            double captainsTwelvePercent = totalAfterCrewMemberTownGold * .12;
+       
+            //Deduct captains twelve percent from the remainder after the crews town gold
+            double afterCaptainsTwelvePercent = totalAfterCrewMemberTownGold - captainsTwelvePercent;
+           
+
+            //Get First Officer's eight percent
+            double firstOfficersEightPercent = afterCaptainsTwelvePercent * .08;
+            
+            
+            //Deduct first officer's eight percent from remaining after deducting the captains 12 percent
+            
+            double totalToDivide = afterCaptainsTwelvePercent - firstOfficersEightPercent;
+            
+            //Divde remaining gold after captain and first officers share by total pirates
+            double crewSubTotal = totalToDivide / totalPirates;
+            //add total amount per crew member to captains twelve percent to get his total
+            double captainsTotalGold = crewSubTotal + captainsTwelvePercent;
+            //add total amount per crew member to first officer's eight percent to get his total
+            double firstOfficersTotalGold = crewSubTotal + firstOfficersEightPercent;
+            //add three to the amount per crew member to get the total amount per crew member
+            double crewMemberTotalGold = crewSubTotal;
+            //Modulus divide the total amount of gold by total number of pirates to get benevolent fund amount
+            double totalBenevolentGold = totalToDivide % totalPirates;
+          
+            
+
+
+            //Display captains gold amount
+            int intCaptainsTotalGold = (int)captainsTotalGold;
+            string totalCaptainsGold = string.Format("{0:0.0}", intCaptainsTotalGold);
+            totalCaptainsGoldTxtBox.Text = totalCaptainsGold;
+
+            //Display first officers gold mount
+            int intFirstOfficersTotalGold = (int)firstOfficersTotalGold;
+            string totalFirstOfficersGold = string.Format("{0:0.0}", intFirstOfficersTotalGold);
+            totalFirstOfficersGoldTxtBox.Text = totalFirstOfficersGold;
+
+            //Display crew member gold amount
+            int intCrewMemberTotalGold = (int)crewMemberTotalGold;
+            string totalCrewMemberGold = string.Format("{0:0.0}", intCrewMemberTotalGold);
+            totalCrewMemberGoldTxtBox.Text = totalCrewMemberGold;
+
+            double ben = (captainsTotalGold - intCaptainsTotalGold) + (firstOfficersTotalGold - intFirstOfficersTotalGold) + ((crewMemberTotalGold *totalCrew) - (intCrewMemberTotalGold * totalCrew));
+
+            //Display benevelont fund gold
+            //string totalBenevolentFundGold = string.Format("{0:0.0}", totalBenevolentGold);
+            //totalBenevolentFundGoldTxtBox.Text = totalBenevolentFundGold;
+
+            string totalBenevolentFundGold = string.Format("{0:0.0}", ben);
+            totalBenevolentFundGoldTxtBox.Text = totalBenevolentFundGold;
+           
+            
+        }
+    }
+}
