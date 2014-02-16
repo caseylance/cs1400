c30e2e8439c8c030b34edbec5e516695891b08c8
caseylance <caseylance@gmail.com>
2014-02-15 21:59:58 -0700

    Project 4

--- Casey-Lancce-Project-4/project4/project4/MainWindow.xaml.cs
+++ Casey-Lancce-Project-4/project4/project4/MainWindow.xaml.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿//Casey Lance
+//CS 1400
+//Project #4
+//---------------------------------------------------------------------
+
+
+// I declare that the following source code was written by me, or provided
+// by the instructor for this project. I understand that copying 
+// source code from any other source constitutes cheating, and that I will
+// receive a zero grade on this project if I am found in violation of
+// this policy.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,7 +32,7 @@ namespace project4
     /// </summary>
     public partial class MainWindow : Window
     {
-        const double MULTIPLICATION_FACTOR = 2;
+        
         
         public MainWindow()
         {
@@ -33,7 +45,7 @@ namespace project4
             double sideA = double.Parse(sideATxtBox.Text);
             double sideB = double.Parse(sideBTxtBox.Text);
             double angleC = double.Parse(angleCTxtBox.Text);
-            double cosC = Math.PI * (angleC / CONVERSION_TO_RADIANS);
+            double cosC = Math.Cos(Math.PI * (angleC / CONVERSION_TO_RADIANS));
             double sideC = CalculateSideC(sideA, sideB, cosC);
             sideCTxtBox.Text = string.Format("{0:f2}", sideC);
 
@@ -41,6 +53,7 @@ namespace project4
         
         static double CalculateSideC(double side1, double side2, double cos1)
         {
+            const double MULTIPLICATION_FACTOR = 2;
             double squaredResult = (side1 * side1) + (side2 * side2) - (MULTIPLICATION_FACTOR * side1 * side2 * cos1);
             double result = Math.Sqrt(squaredResult);
             return result;    
@@ -52,6 +65,19 @@ namespace project4
         }
 
         private void close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            sideATxtBox.Text = string.Empty;
+            sideBTxtBox.Text = string.Empty;
+            sideCTxtBox.Text = string.Empty;
+            angleCTxtBox.Text = string.Empty;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
