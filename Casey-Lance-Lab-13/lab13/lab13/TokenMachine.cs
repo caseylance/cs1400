5df98ec792b5db186ed5e1095b671dae32e3c5ea
caseylance <caseylance@gmail.com>
2014-03-05 20:53:18 -0700

    lab 13

---
+++ Casey-Lance-Lab-13/lab13/lab13/TokenMachine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab13
+{
+    class TokenMachine
+    {
+        //Data members
+        private int quarters = 0;
+        private int tokens = 0;
+
+        //Default constructor
+        //Purpose:  Sets declared variables to default values
+
+        public TokenMachine()
+        {
+            quarters = 0;
+            tokens = 100;
+        }
+          
+    }
+}
