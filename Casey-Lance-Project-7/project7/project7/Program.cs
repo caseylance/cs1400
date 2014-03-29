d83b65dc721b57f4a0f3af8a12628bbe5f2ae567
caseylance <caseylance@gmail.com>
2014-03-29 16:36:44 -0600

    Lab 7

--- Casey-Lance-Project-7/project7/project7/Program.cs
+++ Casey-Lance-Project-7/project7/project7/Program.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿//Casey Lance
+//CS 1400
+//Project #7
+//---------------------------------------------------------------------
+// I declare that the following source code was written by me, or provided
+// by the instructor for this project. I understand that copying 
+// source code from any other source constitutes cheating, and that I will
+// receive a zero grade on this project if I am found in violation of
+// this policy.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
