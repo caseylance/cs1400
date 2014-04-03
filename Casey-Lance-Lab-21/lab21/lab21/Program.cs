f140938ee9bd7f91ab6411cc4b2c02cde871d17e
caseylance <caseylance@gmail.com>
2014-04-02 21:27:20 -0600

    lab 21

---
+++ Casey-Lance-Lab-21/lab21/lab21/Program.cs
@@ -0,0 +1,41 @@
+﻿// File Prologue
+// Name: your name
+// CS 1400 - your section
+// Project: Passing Parameters by value and by reference
+// Date: Today
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // declare some variables to test the program
+        int number1 = 10;
+        int number2 = 15;
+
+        // call the Swap method and output the result
+        Swap(ref number1, ref number2);
+        Console.WriteLine("number1 = {0}", number1);
+        Console.WriteLine("number2 = {0}", number2);
+
+        Console.ReadLine();
+    }
+
+    // The Swap method
+    // Purpose: To interchange the value of two variables
+    // Parameters: two integers
+    // Returns: None
+    // Pre-conditions: None
+    // Post-conditions: None
+    // Side Effect: the values of the 2 integers are swapped
+
+    static void Swap( ref int n1, ref int n2)
+    {
+        int temp = n1;
+        n1 = n2;
+        n2 = temp;
+
+    }
+}
+/* This is necessary because when passing by value the method does not change the value of the variables.  By passing a reference, the values themselves are changes rather than coppies of the values.*/
