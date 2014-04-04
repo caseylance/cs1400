f29be04c28a03f3d6889fc22ba4aa98c82d72a62
caseylance <caseylance@gmail.com>
2014-04-04 13:35:58 -0600

    Lab 22

---
+++ Casey-Lance-Lab-22/lab22/lab22/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an amount of money in dollars and cents:");
+            double userInput = double.Parse(Console.ReadLine());
+            int dollars = 0;
+            int cents = 0;
+            Console.WriteLine("You have {0} dollars and {1} cents.", dollars, cents);
+            Console.ReadLine();
+            
+        }
+    }
+}
