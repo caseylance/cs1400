43f1940c025e253cf535dc88ebf1dac7761de2e7
caseylance <caseylance@gmail.com>
2014-04-07 22:23:09 -0600

    Lab 23

---
+++ Casey-Lance-Lab-23/lab23/lab23/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab23
+{
+    class Program
+    {
+        const int SIZE = 10;
+        static void Main(string[] args)
+        {
+            
+            int[] arrayForLab = new int[SIZE];
+
+            for (int i = 0; i < SIZE; i++)
+            {
+                Console.WriteLine("Enter value {0} in the array:", (i+1));
+                int arrayValue = arrayForLab[i];
+                arrayForLab[i] = int.Parse(Console.ReadLine());
+            }
+
+            foreach (int value in arrayForLab)
+            {
+                Console.Write(" {0}", value);
+            }
+                
+            
+            int valueTotal = TotalArray(arrayForLab);
+
+            Console.WriteLine("\n{0}", valueTotal);
+
+
+            Console.ReadLine();
+       
+              
+        }
+        
+        static int TotalArray (int[] arrayForLab)
+        {
+            int sumOfArrayValues=0;
+            for (int i=0; i < arrayForLab.Length; i++)
+            {
+                sumOfArrayValues += arrayForLab[i]; 
+            }
+            return sumOfArrayValues;
+            
+        }
+    }
+}
