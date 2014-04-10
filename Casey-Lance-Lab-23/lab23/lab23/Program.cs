8fe766f34b709d4ff9620c050b8f0df6e78cc29f
caseylance <caseylance@gmail.com>
2014-04-09 20:48:08 -0600

    lab 23

--- Casey-Lance-Lab-23/lab23/lab23/Program.cs
+++ Casey-Lance-Lab-23/lab23/lab23/Program.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿// Name: Casey Lance
+// CS 1400 - 601
+//Lab 23
+//---------------------------------------------------------------------
+// I declare that the following source code was written by me, or provided
+// by the instructor for this project. I understand that copying 
+// source code from any other source constitutes cheating, and that I will
+// receive a zero grade on this project if I am found in violation of
+// this policy.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,25 +25,24 @@ namespace lab23
             int[] arrayForLab = new int[SIZE];
 
             for (int i = 0; i < SIZE; i++)
-            {
-                Console.WriteLine("Enter value {0} in the array:", (i+1));
+            {   int value = i+1;
+                Console.WriteLine("Enter the {0} value in the array:", ToOrdinal(value));
                 int arrayValue = arrayForLab[i];
                 arrayForLab[i] = int.Parse(Console.ReadLine());
             }
 
+            Console.WriteLine("\nThe values in the array are:");
+
             foreach (int value in arrayForLab)
             {
-                Console.Write(" {0}", value);
+                Console.Write("{0} ", value);
             }
                 
             
             int valueTotal = TotalArray(arrayForLab);
-
-            Console.WriteLine("\n{0}", valueTotal);
-
-
+            Console.WriteLine("\n\nThe sum of the values in the array is:");
+            Console.WriteLine("{0}", valueTotal);
             Console.ReadLine();
-       
               
         }
         
@@ -45,7 +54,59 @@ namespace lab23
                 sumOfArrayValues += arrayForLab[i]; 
             }
             return sumOfArrayValues;
-            
         }
+
+        static string ToOrdinal(int num)
+        {
+            int number = num;
+            const int TEN = 10;
+            string numString = number.ToString();
+            string ordinal = "";
+            if (number == 1)
+            {
+                ordinal = numString + "st";
+            }
+            else if (number == 2)
+            {
+                ordinal = numString + "nd";
+            }
+            else if (number == 3)
+            {
+                ordinal = numString + "rd";
+            }
+            else if (number > 3 && number <= 13)
+            {
+                ordinal = numString + "th";
+            }
+            else if (number >=14)
+            {
+                if (number % TEN == 0)
+                {
+                    ordinal = numString + "th";
+                }
+                else if (number % TEN == 1)
+                {
+                    ordinal = numString + "st";
+                }
+                else if (number % TEN == 2)
+                {
+                    ordinal = numString + "nd";
+                }
+                else if (number % TEN == 3)
+                {
+                    ordinal = numString + "rd";
+                }
+                else
+                {
+                    ordinal = numString + "th";
+                }
+ 
+            }
+
+            return ordinal;
+        }
+
+            
+        
     }
 }
