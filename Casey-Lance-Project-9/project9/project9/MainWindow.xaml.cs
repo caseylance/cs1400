637935bafbc97b8d699238de095f124803de2fb9
caseylance <caseylance@gmail.com>
2014-04-12 20:24:14 -0600

    Project 9

---
+++ Casey-Lance-Project-9/project9/project9/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿//Casey Lance
+//CS 1400
+//Lab #XX
+
+//I declare that the following source code was written by me, or provided
+//by the instructor for this project. I understand that copying 
+//source code from any other source constitutes cheating, and that I will
+//receive a zero grade on this project if I am found in violation of
+// this policy.
+//---------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace project9
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void about_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Casey Lance\nCS1400\nLab xx");
+        }
+
+        private void close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void NameAndScoreTxtBox_TouchEnter(object sender, TouchEventArgs e)
+        {
+            string  userInput = NameAndScoreTxtBox.Text;
+
+            BowlingTeam teamScores = new BowlingTeam();
+            teamScores.SetUserInput(userInput);
+        }
+    }
+
+}
---
+++ Casey-Lance-Project-9/project9/project9/TeamScores.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project9
+{
+    class BowlingTeam
+    {
+        string userInputString = "";
+        string playerName = "";
+        string playerScore = "";
+
+
+
+
+        //Number of players
+        //Array of first names
+        //Array of scores
+
+        //Method to calculate high score
+        //Method to calculate low score
+        //Method to calculate average score
+
+        public void SetUserInput(string input)
+        {
+            userInputString = input;
+        }
+
+
+        string[ ] scoreInfo = userInputString.Split( );
+        // Parse each element of the array into the correct data type
+        string name = scoreInfo[ 0 ] );
+        int score = int.Parse(scoreInfo[ 1 ] );
+
+        public double CalcAverageScore();
+        public int  HighScore();
+        public int LowScore();
+    }
+}
