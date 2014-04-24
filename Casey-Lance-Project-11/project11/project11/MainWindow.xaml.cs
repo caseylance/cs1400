c8f8e1002075ab91f6e102d83d6e2b64a602a809
caseylance <caseylance@gmail.com>
2014-04-24 01:08:43 -0600

    Project 11

---
+++ Casey-Lance-Project-11/project11/project11/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿//Casey Lance
+//CS 1400
+//Lab #XX
+
+//I declare that the following source code was written by me, or provided
+//by the instructor for this project. I understand that copying 
+//source code from any other source constitutes cheating, and that I will
+//receive a zero grade on this project if I am found in violation of
+// this policy.
+//---------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace project11
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void about_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Casey Lance\nCS1400\nLab xx");
+        }
+
+        private void close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
